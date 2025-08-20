402ec238f03009240a75933397b011e2e83759f8
Stacey Kremer <staceybumblebee@gmail.com>
2025-08-20 18:58:03 +0500

    Fix replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterDetails from "./pages/CharacterDetails";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/character/:id" element={<CharacterDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
